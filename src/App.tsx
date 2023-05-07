9adffa32e2691659210e26799623514ce29980e9
Kiril Zyapkov <ziapkov@gmail.com>
2023-05-07 14:18:45 +0300

    configure some files

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
+import { BrowserRouter, Form } from "react-router-dom";
 
+import {
+  Hero,
+  Navbar,
+  About,
+  Tech,
+  Experience,
+  Works,
+  Feedbacks,
+  Contact,
+  StarsCanvas,
+} from "./components";
 
 function App() {
-
-  return (<h1 className='text-red-600'>Hi</h1>  )
+  return <h1 className="text-red-600">Hi</h1>;
 }
 
-export default App
+export default App;
