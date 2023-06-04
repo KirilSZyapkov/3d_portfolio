805534747debc0cc5d7ea301399cbdcbb68dcb04
Kiril Zyapkov <ziapkov@gmail.com>
2023-06-04 16:50:41 +0300

    Styling Earth component.

--- src/components/canvas/Earth.tsx
+++ src/components/canvas/Earth.tsx
@@ -17,9 +17,18 @@ frameloop='demand'
 gl={{preserveDrawingBuffer: true}}
 camera={{}}
 >
+    <Suspense fallback={<CanvasLoader/>}>
+    <OrbitControls 
+    autoRotate
+    enableZoom={false}
+    maxPolarAngle={Math.PI / 2}
+    minPolarAngle={Math.PI / 2}
 
+    />
+    <Earth />
+    </Suspense>
 </Canvas>
   )
 }
 
-export default Earth;
+export default EarthCanvas;
