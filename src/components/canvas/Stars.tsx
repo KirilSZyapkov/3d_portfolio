38c7f00bc727c8090588f103ae6462c41ed77977
Kiril Zyapkov <ziapkov@gmail.com>
2023-06-11 12:04:10 +0300

    Fixing ref bug.

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -4,7 +4,7 @@ import { Points, PointMaterial, Preload } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.cjs";
 
 const Stars = (props: any) => {
-  const ref = useRef();
+  const ref = useRef<any>();
 
   const sphere = random.inSphere(new Float32Array(5000), { radius: 1.2 });
 
